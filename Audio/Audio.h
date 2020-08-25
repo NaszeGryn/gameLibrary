1a1807d321038dd9b17e67267458f107e258b688
NaszeGryn <70233490+NaszeGryn@users.noreply.github.com>
2020-08-25 21:32:35 +0200

    Add files via upload

--- Audio/Audio.h
+++ Audio/Audio.h
@@ -1,1 +1,33 @@
+#pragma once
+#include <SFML/Audio.hpp>
 
+namespace Audio {
+	
+	class Sound {
+	public:
+		Sound(std::string filename, float volume, bool loop) {
+			this->buffer = new sf::SoundBuffer();
+			this->buffer->loadFromFile(filename);
+			this->sound = new sf::Sound(*this->buffer);
+			this->sound->setVolume(volume);
+			this->sound->setLoop(loop);
+		}
+		~Sound() {
+			delete this->sound;
+			delete this->buffer;
+			this->buffer = NULL;
+			this->sound = NULL;
+		}
+
+		void setVolume(float volume) { this->sound->setVolume(volume); }
+
+		void play() { this->sound->play(); }
+		void stop() { this->sound->stop(); }
+
+		sf::Sound* getSound() { return this->sound; }
+		sf::SoundBuffer* getBuffer() { return this->buffer; }
+	private:
+		sf::Sound* sound;
+		sf::SoundBuffer* buffer;
+	};
+}
