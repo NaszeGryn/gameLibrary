893f0f6bc16e990084563aa9febfa8cf2551ef6b
NaszeGryn <70233490+NaszeGryn@users.noreply.github.com>
2020-09-28 21:47:49 +0200

    Add files via upload

--- Graphics/Graphics.h
+++ Graphics/Graphics.h
@@ -4,5 +4,7 @@
 #include "GraphicsProcessing.h"
 #include "Particles.h"
 
+#define rgb(r, g, b) sf::Color(r, g, b)
+#define rgba(r, g, b, a) sf::Color(r, g, b, a)
 #define gL_fullScreen sf::VideoMode::getDesktopMode()
 #define gL_fullScreenSize sf::Vector2f(sf::VideoMode::getDesktopMode().width, sf::VideoMode::getDesktopMode().height)
